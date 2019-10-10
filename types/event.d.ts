1cf72290c365412973542f75c6f63f96a0ae1e25
hxc <742037091@qq.com>
2019-10-10 15:22:45 +0800

    fix: fix event.t.ds file

---
+++ types/event.d.ts
@@ -0,0 +1,13 @@
+// 视频组件事件
+export declare enum videoEvents {
+  onLoadstart = 'onLoadstart',
+  onCanplay = 'onCanplay',
+  onPlay = 'onPlay',
+  onPause = 'onPause',
+  onPlaying = 'onPlaying',
+  onTimeupdate = 'onTimeupdate',
+  onWaiting = 'onWaiting',
+  onEnded = 'onEnded',
+  onError = 'onError',
+  onFullscreenChange = 'onFullscreenChange'
+}
